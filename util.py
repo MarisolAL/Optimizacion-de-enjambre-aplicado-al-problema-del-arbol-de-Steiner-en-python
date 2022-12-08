4ff2c043c36ddd3f55e6091db663c2078bfef90c
Marisol <marisolamezcua@ciencias.unam.mx>
2022-12-08 17:45:24 -0600

    Agrega funciones de utilidad

---
+++ util.py
@@ -0,0 +1,9 @@
+import math
+
+
+def distancia_entre_dos_puntos(punto_1, punto_2):
+    """Función que calcula la distancia entre dos puntos con dimensión n."""
+    distancia = 0
+    for i in range(0, len(punto_1)):
+        distancia += pow((punto_2[i] - punto_1[i]), 2)
+    return math.sqrt(distancia)
