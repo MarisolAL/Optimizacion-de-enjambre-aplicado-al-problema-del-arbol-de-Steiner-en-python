a3c57a2ccd2b587edbc1fe412805459f3fa68529
Marisol <marisolamezcua@ciencias.unam.mx>
2022-12-22 19:02:24 -0600

    WIP: Clase Steiner

--- src/steiner.py
+++ src/steiner.py
@@ -3,13 +3,31 @@ import src.enjambre as enjambre
 from src.util import distancia_entre_dos_puntos
 
 class Steiner:
+    """
+    Clase que modela el problema del árbol de Steiner
+
+    Attributes
+    ----------
+    puntos: list
+        Puntos pertenecientes al conjunto inicial del problema
+    arbol: list
+        Lista de aristas pertenecientes al árbol euclideano de peso mínimo
+    peso: float
+        Número con el valor del peso del árbol
+    """
 
     def __init__(self, puntos):
         self.puntos = puntos
-        self.arbol = self.obten_arbol
-        self.peso = self.obten_peso_total(self.arbol)
+        self.arbol = None
+        self.peso = None
 
-    def obten_arbol(self):
+    def calcula_arbol_euclidiano_minimo(self):
+        """
+        Función que calcula dado el conjunto de puntos inicial el arbol generador de peso
+        mínimo euclidiano y lo regresa.
+        :return: list
+            Regresa la lista de aristas que forman el árbol de peso mínimo euclidiano
+        """
         grafica = nx.Graph()
         for punto_i in self.puntos:
             grafica.add_node(punto_i)
@@ -23,3 +41,20 @@ class Steiner:
             arbol.add_edge(arista_i[0], arista_i[1], weight=peso)
         return arbol
 
+    def obten_peso_total_arbol(self):
+        """
+        Función que calcula el peso total del árbol en el conjunto de puntos.
+        :return: float
+            Suma de los pesos de las aristas del árbol
+        """
+        peso_total = 0
+        print(self.arbol)
+        for arista in self.arbol.edges.data('weight', default=1000):
+            print(arista)
+            peso_total += arista[2]
+        return peso_total
+
+steiner = Steiner([(-8,5),(10,2),(50,-20),(1,24)])
+print(steiner.arbol)
+arbol = steiner.calcula_arbol_euclidiano_minimo()
+print(arbol)
